b773dc9aea959cc57e5950c382248567e3dba3c9
Celso Crivelaro <celso.crivelaro@revelo.com.br>
2021-03-19 19:17:29 -0300

    Adicionando classe abstrata

---
+++ individuo.py
@@ -0,0 +1,14 @@
+class Individuo():
+  def fitness(self) -> float:
+    pass
+
+  @classmethod
+  def random_instance(cls):
+    pass
+
+  def crossover(self, outro):
+    pass
+
+  def mutate(self):
+    pass
+
